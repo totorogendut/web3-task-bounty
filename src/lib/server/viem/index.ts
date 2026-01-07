dc0673aab73e8e5a3898a19d41ac6bc354a4fc4b
ProgNovel <prognovel@gmail.com>
2026-01-07 20:10:14 +0800

    refactor contract location

--- src/lib/server/viem/index.ts
+++ src/lib/server/viem/index.ts
@@ -2,34 +2,8 @@ import { PUBLIC_PLATFORM_ADDRESS } from "$env/static/public";
 import { createPublicClient, createWalletClient, erc20Abi, http, parseEther } from "viem";
 import { ethChain, MneeTokenAddress } from "../../_eth-shared";
 import { privateKeyToAccount } from "viem/accounts";
-import { WALLET_PRIVATE_KEY } from "$env/static/private";
 
 const client = createPublicClient({
 	chain: ethChain,
 	transport: http(),
 });
-
-const projectWallet = createWalletClient({
-	account: privateKeyToAccount(`0x${WALLET_PRIVATE_KEY}`),
-	chain: ethChain,
-	transport: http(),
-});
-
-export const getPublicWalletFund = async () =>
-	client.readContract({
-		address: MneeTokenAddress,
-		abi: erc20Abi,
-		functionName: "balanceOf",
-		args: [PUBLIC_PLATFORM_ADDRESS as `0x${string}`],
-	});
-
-export const transferFund = async (to: `0x${string}`, amount: string) => {
-	const tx = await projectWallet.writeContract({
-		address: MneeTokenAddress,
-		abi: erc20Abi,
-		functionName: "transfer",
-		args: [to, parseEther(amount)],
-	});
-
-	return tx;
-};
