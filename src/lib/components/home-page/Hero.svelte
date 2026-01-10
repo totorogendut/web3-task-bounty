8091e61ff4df13aeca4009e3aa7ff2281e453afe
ProgNovel <prognovel@gmail.com>
2026-01-10 18:52:53 +0800

    hero image fix cls

--- src/lib/components/home-page/Hero.svelte
+++ src/lib/components/home-page/Hero.svelte
@@ -4,7 +4,7 @@
 
 <section class="mx-auto mt-24 flex w-300 max-w-full">
 	<div class="relative w-120">
-		<img class="absolute -bottom-14 left-4 -z-10 rotate-14" src="/arrow.webp" alt="Arrow." />
+		<img class="absolute -bottom-14 left-4 -z-10 h-125 rotate-14" src="/arrow.webp" alt="Arrow." />
 		<h1 class="title rye-regular text-6xl! font-extrabold">{title}</h1>
 		<strong class="absolute mt-4 pr-2 text-lg leading-[1.2] font-semibold opacity-80"
 			>Post a work as bounty. Or earn as a freelancer by completing a bounty. Where bounty bound by
