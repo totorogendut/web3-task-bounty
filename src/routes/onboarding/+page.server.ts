8882b72b102363f07717a7b190dde6f43ca2389b
ProgNovel <prognovel@gmail.com>
2026-01-10 18:50:23 +0800

    remove sharp from import

--- src/routes/onboarding/+page.server.ts
+++ src/routes/onboarding/+page.server.ts
@@ -1,6 +1,6 @@
 import type { Actions } from "../$types";
-import sharp from "sharp";
-import { uploadFiles } from "$lib/server/attachments";
+// import sharp from "sharp";
+// import { uploadFiles } from "$lib/server/attachments";
 import { db } from "$lib/server/db";
 import { user as userSchema } from "$lib/server/db/schemas/users";
 import { eq } from "drizzle-orm";
@@ -21,18 +21,3 @@ export const actions: Actions = {
 		return redirect(302, "/");
 	},
 };
-
-async function uploadAvatar(file: File, userId: string) {
-	const avatarBufferPng = await sharp(await file.arrayBuffer())
-		.resize({ width: 64, height: 64 })
-		.png()
-		.toBuffer();
-	const avatarBufferWebp = await sharp(avatarBufferPng).webp({ quality: 80 }).toBuffer();
-	const pngBlob = new Blob([Buffer.from(avatarBufferPng)], { type: "image/png" });
-	const webpBlob = new Blob([Buffer.from(avatarBufferWebp)], { type: "image/webp" });
-
-	await uploadFiles("avatar", [
-		new File([pngBlob], `${userId}.png`, { type: "image/png" }),
-		new File([webpBlob], `${userId}.webp`, { type: "image/webp" }),
-	]);
-}
